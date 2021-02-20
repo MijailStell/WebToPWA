b6abd74b711337cae1c7fc06d65c63f387fc7da8
MijailStell <mijailstell@gmail.com>
2021-02-20 14:43:01 -0500

    config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiKey: 'AIzaSyArH-JU-dtgamU1QKrn4STcnsOzmzhUzBo',
   timeOutNotifications: 2000,
-  urlBaseServiciosApi: 'http://localhost:4000',
+  urlBaseServiciosApi: 'https://playlist-pwa.herokuapp.com',
   VAPID_PUBLIC_KEY: 'BAkNsj2kN7ZYEbdKHDJwvEhuaeT6GJB9FYRbPHEFYSBHxxy8Zm2-k9Xmcbv20Z3kYsOdmcIODe9yH4h4CtVRCBQ'
 };
 
